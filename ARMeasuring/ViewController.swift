b8e9af203206b302b5def5b79845e351ba1eeb64
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-01 20:09:07 -0300

    Add a sphere node on tap right on where the camera is (but you can only see it if you go back a little)

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -20,7 +20,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = transform
+        self.sceneView.scene.rootNode.addChildNode(sphere)
     }
 
     override func didReceiveMemoryWarning() {
