cb7d8fab7cad505d40818dd4f2b2c39ebe1a5456
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-01 20:00:18 -0300

    Set up initial scene view, stack labels and add button

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
