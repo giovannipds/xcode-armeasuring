1e1a8e6b3cd7b732051c9d301fa7f671b55d5f56
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-01 21:48:16 -0300

    Write the logic to calc the distance and update the labels text (though it doesn't work because it's on the background thread)

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import ARKit
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var zLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var distance: UILabel!
+    var startingPosition: SCNNode?
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,7 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration)
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -32,18 +34,29 @@ class ViewController: UIViewController {
         let transform = camera.transform
         var translationMatrix = matrix_identity_float4x4
         translationMatrix.columns.3.z = -0.1
-        var modifiedMatrix = simd_mul(transform, translationMatrix)
+        let modifiedMatrix = simd_mul(transform, translationMatrix)
         let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
         sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
         sphere.simdTransform = modifiedMatrix
         self.sceneView.scene.rootNode.addChildNode(sphere)
+        self.startingPosition = sphere
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let startingPosition = self.startingPosition else {return}
+        guard let pointOfView = sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let xDistance = location.x - startingPosition.position.x
+        let yDistance = location.y - startingPosition.position.y
+        let zDistance = location.z - startingPosition.position.z
+        self.xLabel.text = "\(xDistance)"
+        self.yLabel.text = "\(yDistance)"
+        self.zLabel.text = "\(zDistance)"
+    }
 
 }
-
